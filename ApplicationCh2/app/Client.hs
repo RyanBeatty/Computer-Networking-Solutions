45eb91a94c8f58b12fac4910f6767555dcb9bc35
Ryan Beatty <rvbeatty@email.wm.edu>
2017-07-09 17:25:59 -0700

    Added initial Client.hs file. Need to implement

---
+++ ApplicationCh2/app/Client.hs
@@ -0,0 +1,38 @@
+module Main where
+
+import Control.Monad
+import Network.Socket
+import System.Directory
+
+import Lib
+
+-- Service incoming requests for files. I'm just trying to get a feel for
+-- networking, so I didn't implement any sort of parsing of messages or
+-- proper HTTP responses so I just assume every request is a uri for a file.
+mainLoop :: Socket -> IO ()
+mainLoop socket =
+  forever $ do
+    (conn, _) <- accept socket
+    filename <- recv conn 1024
+    print $ "Received request for file: " ++ filename
+    isfile <- doesFileExist filename
+    if isfile then
+      -- Send the file.
+      do file <- readFile filename
+         send conn file -- Assuming that all data gets sent here.
+         close conn
+    else
+      -- Error 404.
+      do send conn ("Error 404 Not Found\r\n")
+         close conn
+
+main :: IO ()
+main = do
+  print "Starting Server"
+  listen_socket <- socket AF_INET Stream defaultProtocol
+  bind listen_socket (SockAddrInet 2020 iNADDR_ANY)
+  listen listen_socket 2
+  print "Ready to Accept Connections"
+  mainLoop listen_socket
+  print "Server Shutting Down"
+  close listen_socket
