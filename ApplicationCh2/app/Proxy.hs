293bc5b2b5d6bf5224f46df3f8dac35fbb611f0a
rbeatty <rbeatty@rbeatty-glaptop0.roam.corp.google.com>
2017-07-15 16:59:37 -0700

    wrote some initial code. Needs lots of cleanup

--- ApplicationCh2/app/Proxy.hs
+++ ApplicationCh2/app/Proxy.hs
@@ -3,7 +3,6 @@ module Main where
 import Control.Concurrent
 import Control.Monad
 import Data.String.Utils
-import Network
 import Network.Socket
 import System.Directory
 
@@ -49,6 +48,18 @@ import Lib
 --  child <- spawnChild conn
 --  mainLoop socket (child:children)
 
+mainLoop :: Socket -> IO ()
+mainLoop sock = do
+  (clientconn, _) <- accept sock
+  (serverconn, addr) <- makeSocket "www.google.com" "80"
+  connect serverconn (addrAddress addr)
+  request <- recv clientconn 1024
+  send serverconn request
+  response <- recv serverconn 1024
+  send clientconn response
+  close serverconn
+  close clientconn
+
 makeSocket :: HostName -> ServiceName -> IO (Socket, AddrInfo)
 makeSocket hostname port = do
   let hints = defaultHints { addrSocketType = Stream, addrFamily = AF_INET }
@@ -63,5 +74,6 @@ main = do
   bind sock (addrAddress addr)
   listen sock 2
   print "Listening on port 2020"
+  mainLoop sock
   print "Server Shutting Down"
   close sock
